dc42446b08950837f7e51dfe51d489aba0ca2f45
osawa-koki <github@osawa.dev>
2023-07-24 22:56:11 +0900

    プロジェクトの初期化。

---
+++ index.rb
@@ -0,0 +1,19 @@
+require 'erb'
+require 'yaml'
+
+# YAMLファイルを読み込む
+config = YAML.load_file('config.yml')['julia']
+
+# マンデルブロ集合の設定を取得する
+width = config['width']
+height = config['height']
+x_min = config['x_min']
+x_max = config['x_max']
+y_min = config['y_min']
+y_max = config['y_max']
+cx = config['cx']
+cy = config['cy']
+max_iterations = config['max_iterations']
+incremental_step = config['incremental_step']
+
+File.open('./www/index.html', 'w').write(ERB.new(File.read("index.html.erb")).result(binding))
